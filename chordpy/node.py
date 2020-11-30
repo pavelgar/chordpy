cc9c52deea9460ad3c8e279d11f118cd95ff73e8
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2020-11-30 14:26:04 +0200

    Working messages

--- chordpy/node.py
+++ chordpy/node.py
@@ -2,7 +2,6 @@ import time
 import types
 import socket
 import selectors
-from selectors import EVENT_READ, EVENT_WRITE
 
 
 class Node:
@@ -29,10 +28,8 @@ class Node:
         remote, port = dest
         remote_ip = socket.gethostbyname(remote)
         print(f"Connecting to ring at {dest}")
-
-        if remote_ip == self.ip:
-            self.successor = (self.ip, self.id)
-        else:
+        remote_id = self.id
+        if remote_ip != self.ip:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             payload = f"key:{self.id}".encode("utf-8")
             retry_count = 0
@@ -51,10 +48,14 @@ class Node:
                     print(f"Retrying to join... (count: {retry_count})")
 
             sock.close()
-            if not data:
-                raise Exception("Could not connect to destination!")
-            # Save the remote's id
-            self.successor = (remote_ip, int(data))  # Tuple of (address, id)
+            if data:
+                remote_id = int(data)
+            else:
+                raise Exception("Host didn't send it's key!")
+
+            # Save my successor
+        self.successor = (remote_id, remote_ip)
+        print(f"Succesfully joined the ring at {dest}.")
 
     def run(self):
         """
@@ -63,6 +64,7 @@ class Node:
         print("Starting event loop")
         try:
             while True:  # Main event loop
+                # Wait for new event(s)
                 events = self.slctrs.select(timeout=None)
                 for key, mask in events:
                     if key.data is None:
@@ -71,8 +73,8 @@ class Node:
                     else:
                         # Respond to existing connection
                         self.respond(key, mask)
-        except KeyboardInterrupt:
-            print("\nInterrupted, exiting...")
+        except Exception as e:
+            print("\nInterrupted, exiting...", e)
         finally:
             self.slctrs.close()
 
@@ -83,43 +85,54 @@ class Node:
         conn, addr = sock.accept()
         conn.setblocking(False)
         print("New connection from", addr)
-        data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-        events = EVENT_READ | EVENT_WRITE
+        data = types.SimpleNamespace(addr=addr, msg=b"", cmd="", done=False)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE  # Possible events
         self.slctrs.register(conn, events, data=data)
 
     def respond(self, key, mask):
         sock = key.fileobj
         data = key.data
-        if mask & EVENT_READ:
+        if mask & selectors.EVENT_READ:
             recv_data = sock.recv(1024)
             if recv_data:
-                data.outb += recv_data
-            else:
-                print("closing connection to", data.addr)
+                # Extract the "command" of this message.
+                # It's always the first few bytes of a message before a colon character
+                if not data.cmd:
+                    data.cmd, msg = recv_data.decode("utf-8").split(":", 1)
+                    data.msg = msg.encode("utf-8")
+                # If the cmd is already set, simply store the rest of the message
+                else:
+                    data.msg += recv_data
+
+                if len(recv_data) < 1024:  # Check if this was the last "chunk"
+                    data.done = True
+
+            else:  # No more data to read
+                print("Closing connection to", data.addr, "(end of data)")
                 self.slctrs.unregister(sock)
                 sock.close()
 
-        if mask & EVENT_WRITE:
-            if data.outb:
-                print("Responding with", repr(data.outb), "to", data.addr)
-                sent = sock.send(data.outb)
-                data.outb = data.outb[sent:]
+        if (mask & selectors.EVENT_WRITE) and data.done:  # Check also for done reading
+            msg = data.msg.decode("utf-8")
+            res = self.handle_message(data.cmd, msg)
+            sock.send(res.encode("utf-8"))
+            data.done = False  # Marks this message as "processed"
 
-    def handle_message(self, key, data):
+    def handle_message(self, cmd: str, message: str) -> str:
         """
         Top-level function to determine how to handle each type of message that this node receives.
 
         Return value is a message that is a response to the received message.
         """
-        if key == "key":
+        if cmd == "key":
             # Send my key to the requester
-            return self.id
-        elif key == "notify":
+            return str(self.id)
+        elif cmd == "notify":
             #
             pass
-        elif key == "request":
+        elif cmd == "request":
             pass
-        elif key == "status":
+        elif cmd == "status":
             pass
 
     def stabilize(self, pred, id, succ):
